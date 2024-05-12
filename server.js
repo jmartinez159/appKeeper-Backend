08229d30290091d31227d49b033d609d86c9171c
Jorge <jmx012@gmail.com>
2024-05-12 03:36:02 -0700

    Added CORS policies and POST route to get URLs

--- server.js
+++ server.js
@@ -2,9 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define a route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Set CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with specific origins if needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//Middleware to parse json
+app.use(express.json());
+
+// Your other routes and logic here
+
+// Default route
+// app.get('/', (req, res) => {
+
+//     console.log('GET route activated');
+//     res.send({'message': 'Hello from Backend'});
+// });
+
+// This route checks if url in req is in the backend or not
+app.post('/', (req, res) => {
+
+    console.log('POST route activated');
+    console.log(req.body);
+    res.send({'message': 'POST Request received'});
 });
 
 // Start the server
